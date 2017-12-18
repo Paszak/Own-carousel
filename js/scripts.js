bbd12b31e916e0508c818531e0c73e46815aebd1
Paweł Koleśnik <pawelkolesnik@wp.pl>
2017-12-18 21:25:30 +0100

    problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,47 @@
 $(function(){
 
-var carouselList = $('#carousel ul');
+    var carouselList = $('#carousel ul');
 
-setInterval(changeSlide, 3000);
+    var myTimer = setInterval(changeSlide, 5000);
 
-function changeSlide() {
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-}
+    function changeSlide() {
+        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    }
 
-function moveFirstSlide() {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
-}
+    function prevSlide() {
+        carouselList.animate({'marginRight':-400}, 500, moveLastSlide);
+    }
+
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+        // var activeDot = dots.find("")
+    }
+
+    function moveLastSlide() {
+        var firstItem = carouselList.find("li:last");
+        var lastItem = carouselList.find("li:first");
+        lastItem.before(firstItem);
+        carouselList.css({marginRight:0});
+    }
+
+    var nextButton = $('.next'),
+        prevButton = $('.prev');
+
+    nextButton.click(function(event) {
+        clearInterval(myTimer);
+        changeSlide();
+        myTimer = setInterval(changeSlide, 5000);
+    });
+
+    prevButton.click(function(event) {
+        clearInterval(myTimer);
+        prevSlide();
+        myTimer = setInterval(changeSlide, 5000);
+    });
+
+    // var dots = $('.indicators');
 
 });
